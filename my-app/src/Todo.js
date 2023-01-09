e744c549409a6da0dbaba2cf9c925083ed18fc7d
ofir6283 <93862727+ofir6283@users.noreply.github.com>
2023-01-09 11:39:03 +0200

    logics added. css need to be added.

---
+++ my-app/src/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoClick() {
+        toggleTodo(todo.id);
+    }
+    return (
+        <div>
+            <label>
+                {todo.name}
+                <input type='checkbox' checked={todo.isCompleted} onChange={handleTodoClick} />
+            </label>
+        </div>
+    )
+}
